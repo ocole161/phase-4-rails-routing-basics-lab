2098dba427f11bf39cba2e9710f4c15778311705
Owen <ocole161@gmail.com>
2023-02-21 15:54:02 -0700

    commit tree e0110d6ee3a0266193d55f6e0ecd7ffc858f3467

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,9 @@
+class StudentsController < ApplicationController
+    def index
+        render json: Student.all
+    end
+
+    def grades
+        render json: Student.order(grade: :desc)
+    end
+end
